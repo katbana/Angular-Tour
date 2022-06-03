7e5ad96d8a8021d6b5aa2024bbb4a3664aedb7dc
katbana <katleomaloka@gmail.com>
2022-06-03 15:03:10 +0200

    1 file modified

--- src/app/product-child/product-child.component.ts
+++ src/app/product-child/product-child.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-child',
   templateUrl: './product-child.component.html',
-  styleUrls: ['./product-child.component.css']
+  styleUrls: ['./product-child.component.css'],
 })
 export class ProductChildComponent implements OnInit {
+  @Input() product: Product | undefined;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
